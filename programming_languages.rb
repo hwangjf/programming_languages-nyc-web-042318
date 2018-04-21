a40dd114dff10cf07565a78ae18cd366fb8b2bdb
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 03:36:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,20 +1,34 @@
 def reformat_languages(languages)
   new_hash = {}
+  a = languages.values
   type_string = ""
   style_array = []
-  arr = languages[:oo].keys + languages[:functional].keys
+  oo_langs = languages[:oo].keys
+  func_langs = languages[:functional].keys
+  arr = oo_langs + func_langs
   language_array = arr.uniq
   languages.each do |style, name|
     name.each do |name, type|
       type.each do |type, type_name|
-        for lang in language_array
-          new_hash[lang] = {
-            :type => type_string,
-            :style => style_array
-          }
+        if name == :ruby || name == :javascript || name == :python
+          type_string = "interpreted"
+        else 
+          type_string = "compiled"
+        end
+        if name == :javascript
+          style_array = [:oo, :functional]
+        else if name == :ruby || name == :python || name == :java
+          style_array = [:oo]
+        else 
+          style_array = [:functional]
+        end
+        new_hash[name] = {
+          :type => type_string,
+          :style => style_array
+        }
         end
       end
     end
   end
+  new_hash
 end
-
