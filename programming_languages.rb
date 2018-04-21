0331ea3e3bb62204346db0dfe75d403d0c44f1b0
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 01:34:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,10 @@ def reformat_languages(languages)
   arr = languages[:oo].keys + languages[:functional].keys
   language_array = arr.uniq
   languages.each do |style, name|
-    name.each do |type, type_name|
-      puts type
+    name.each do |name, type|
+      type.each do |type, type_name|
+        puts type_name
+      end
     end
   end
 end
